3b5fdec618f1d283b1bb0a96ee7549ec42823022
LHT082024 <LHT082024@gmail.com>
2024-11-04 11:14:53 +0100

    added dbcontext

--- models/DbContextClass.cs
+++ models/DbContextClass.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Møteplanlegger.models;
 
 namespace Møteplanlegger
 {
     public class DbContextClass : DbContext
     {
+        public DbSet<People> Peoples { get; set; }
+        public DbSet<Meeting> Meetings { get; set; }
 
     }
 }
