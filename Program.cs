a07e4463ef4a47f77954901d6610c3d1d878d041
LHT082024 <LHT082024@gmail.com>
2024-11-04 14:28:06 +0100

    added delete method

--- Program.cs
+++ Program.cs
@@ -9,22 +9,22 @@ var app = builder.Build();
 
 //these two get methods defines the route for the GET request It will take the People/Meeting object from the dbcontextclass
 //Then convert the objects to a list of People/Meeting-objects and then return the list
-app.MapGet("/People", async (DbContextClass db) =>
+app.MapGet("/people", async (DbContextClass db) =>
   await db.peoples.ToListAsync());
 
-app.MapGet("/Meeting", async (DbContextClass db) =>
+app.MapGet("/meeting", async (DbContextClass db) =>
 await db.meetings.ToListAsync());
 
 //These two get methods once again defines the route for the GET request
 //but this time we can be more specific instead of getting a list of all the meetings/people
 //we instead can get a specific meeting or person. 
-app.MapGet("/People/{id}", async (int id, DbContextClass db) =>
+app.MapGet("/people/{id}", async (int id, DbContextClass db) =>
 await db.peoples.FindAsync(id)
 is People people
 ? Results.Ok(people)
 : Results.NotFound());
 
-app.MapGet("/Meeting/{id}", async (int id, DbContextClass db) =>
+app.MapGet("/meeting/{id}", async (int id, DbContextClass db) =>
 await db.meetings.FindAsync(id)
 is Meeting meeting
 ? Results.Ok(meeting)
@@ -33,25 +33,25 @@ is Meeting meeting
 //this two methods creates a new people/meething object. First it identfies the path in the database
 //then it creates the new people/meeting object and places it into it's assigned space
 //in the database
-app.MapPost("/People", async (People people, DbContextClass db) =>
+app.MapPost("/people", async (People people, DbContextClass db) =>
 {
     db.peoples.Add(people);
     await db.SaveChangesAsync();
 
-    return Results.Created($"/People/{people.EmployeeNumber}", people);
+    return Results.Created($"/people/{people.EmployeeNumber}", people);
 });
 
-app.MapPost("/Meeting", async (Meeting meeting, DbContextClass db) =>
+app.MapPost("/meeting", async (Meeting meeting, DbContextClass db) =>
 {
     db.meetings.Add(meeting);
     await db.SaveChangesAsync();
 
-    return Results.Created($"/People/{meeting.MeetingId}", meeting);
+    return Results.Created($"/people/{meeting.MeetingId}", meeting);
 });
 
 
 //these methods finds a meeting/person by id and then gives you the abilty to update that meeting/person
-app.MapPut("/People/{id}", async (int id, People inputPeople, DbContextClass db) =>
+app.MapPut("/people/{id}", async (int id, People inputPeople, DbContextClass db) =>
 {
     var people = await db.peoples.FindAsync(id);
 
@@ -63,7 +63,7 @@ app.MapPut("/People/{id}", async (int id, People inputPeople, DbContextClass db)
     return Results.NoContent();
 });
 
-app.MapPut("/Meeting/{id}", async (int id, Meeting inputMeeting, DbContextClass db) =>
+app.MapPut("/meeting/{id}", async (int id, Meeting inputMeeting, DbContextClass db) =>
 {
     var meeting = await db.meetings.FindAsync(id);
 
@@ -73,6 +73,33 @@ app.MapPut("/Meeting/{id}", async (int id, Meeting inputMeeting, DbContextClass 
 
     await db.SaveChangesAsync();
     return Results.NoContent();
+});
+
+//this is the final method for people and meeting where you delete them.
+//you first find the path of the item via id, then checks if the item exists and if it does
+//you then have the ability to delete the item.
+app.MapDelete("/people/{id}", async (int id, DbContextClass db) =>
+{
+    if (await db.peoples.FindAsync(id) is People people)
+    {
+        db.peoples.Remove(people);
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+    }
+
+    return Results.NotFound();
+});
+
+app.MapDelete("/meeting/{id}", async (int id, DbContextClass db) =>
+{
+    if (await db.meetings.FindAsync(id) is Meeting meeting)
+    {
+        db.meetings.Remove(meeting);
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+    }
+
+    return Results.NotFound();
 });
 
 
